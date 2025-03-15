7ff8f60e3df111d25f878f3527de496267cdb3e8
Bogdan282 <bogdan673876@gmail.com>
2025-03-15 16:42:16 +0200

    1

---
+++ src/main/numSum.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.util.Scanner;
+
+public class numSum {
+    //22. **Обчислення суми чисел у тексті:**
+    //    Розбийте рядок на слова, перетворіть ті, що є числами (за допомогою `isdigit()`), і порахуйте їхню суму.
+
+    public static void main(String[] args) {
+        String stringNum;
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Введіть рядок: ");
+        stringNum = scn.nextLine();
+        System.out.println("Сума чисел з рядка: " + numSum(stringNum));
+    }
+
+    public static int numSum(String stringNum) {
+        char[] numSumArr = stringNum.toCharArray();
+        int numSum = 0;
+        for (int i = 0; i < numSumArr.length; i++) {
+            if(Character.isDigit(numSumArr[i])) {
+                numSum += numSumArr[i] - '0';
+            }
+        }
+        return numSum;
+    }
+}
