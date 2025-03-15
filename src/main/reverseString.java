4a75fd174bf22fbf13fdd1e06383712632ec9e5a
Bogdan282 <bogdan673876@gmail.com>
2025-03-15 12:28:29 +0200

    1

---
+++ src/main/reverseString.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.Scanner;
+
+public class reverseString {
+    //11. **Реверс порядку слів:**
+    //    Розбийте речення на слова, а потім створіть новий рядок, вставляючи слова у зворотному порядку, зберігаючи порядок символів у кожному слові.
+    public static void main(String[] args) {
+        String words;
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Введите строку: ");
+        words = scn.nextLine();
+        System.out.println("Строка в обратном порядке: " + reverse(words));
+    }
+    public static String reverse(String words) {
+        String reverseString = "";
+        String wordsStringMass[] = words.split(" ");
+        for (int i = wordsStringMass.length - 1; i >= 0; i--) {
+            reverseString += (" " + wordsStringMass[i]);
+        }
+        return reverseString;
+    }
+}
