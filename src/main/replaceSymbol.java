c9748213bf297a45525944823cd59d1fca64bd27
Bogdan282 <bogdan673876@gmail.com>
2025-03-16 11:07:09 +0200

    1

---
+++ src/main/replaceSymbol.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.Scanner;
+
+public class replaceSymbol {
+    //47. **Заміна спеціальних символів на текстові сутності:**
+    //    Використовуйте послідовні виклики `replace`, щоб замінити символи, такі як `&`, `<` та `>`, на їх відповідні текстові сутності.
+    public static void main(String[] args) {
+        String stringWord;
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Введіть рядок: ");
+        stringWord = scn.nextLine();
+        System.out.println("Відредагований рядок: " + symbolReplace(stringWord));
+    }
+    public static String symbolReplace(String stringWord) {
+        return stringWord.replace("<", " менше ").replace(">", " більше ".replace("&", "і"));
+    }
+}
