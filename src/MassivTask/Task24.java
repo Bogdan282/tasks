8f4359065fce96f14cbbb9e79661074ec0ec62bc
Bogdan282 <bogdan673876@gmail.com>
2025-03-19 17:46:01 +0200

    1

---
+++ src/MassivTask/Task24.java
@@ -0,0 +1,20 @@
+package MassivTask;
+
+public class Task24 {
+    //24. Напишите программу, которая создает двумерный массив и выводит его на экран.
+    public static void main(String[] args) {
+        int[][] numArr = new int[5][5];
+
+        for(int i = 0; i < numArr.length; i++) {
+            for (int j = 0; j < numArr[i].length; j++) {
+                numArr[i][j] = (int) (Math.random() * 10);
+            }
+        }
+        for(int i = 0; i < numArr.length; i++) {
+            for (int j = 0; j < numArr[i].length; j++) {
+                System.out.print(numArr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
